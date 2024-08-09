26a53a1819f237f4bb02d9224914e3265ef62126
Kotmail <ya.kotmail@yandex.ru>
2024-08-09 19:40:09 +0300

    Minor changes in the service and the DTO of the Task entity.

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -9,6 +9,8 @@ import {
 } from 'class-validator'
 import { TaskStatus } from '../task.entity'
 import { PriorityEnum } from 'src/shared/enums/priority.enum'
+import { Transform } from 'class-transformer'
+import { User } from 'src/users/user.entity'
 
 export class CreateTaskDto {
   @IsString()
@@ -34,13 +36,24 @@ export class CreateTaskDto {
 
   @IsOptional()
   @IsNumber()
-  creatorId: number
+  @Transform(({ value }) =>
+    typeof value === 'object' && value.id ? value.id : value,
+  )
+  creator: number
 
   @IsNumber()
-  projectId: number
+  @Transform(({ value }) =>
+    typeof value === 'object' && value.id ? value.id : value,
+  )
+  project: number
 
   @IsOptional()
   @IsArray()
   @IsNumber({}, { each: true })
-  responsibleUserIds: number[]
+  @Transform(({ value }) =>
+    value.map((item: number | User) =>
+      typeof item === 'object' ? item.id : item,
+    ),
+  )
+  responsibleUsers: number[]
 }
